e17dbc6d4606959efc6e6149249c47a38cd5bbd8
rainswerld <m.rainrobinson@gmail.com>
2020-09-16 13:30:37 -0600

    Add path to '/new-artist' to create new artist

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 urlpatterns = [
   	# Restful routing
     path('artists/', Artists.as_view(), name='artists'),
+    path('new-artist/', Artists.as_view(), name='new-artist'),
     path('artists/<int:pk>/', ArtistDetail.as_view(), name='artist_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
